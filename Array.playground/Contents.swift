7acf6d3b7e1ed62b775a8d6b93bffb46374b6410
student09 <kannikar@snru.ac.th>
2017-06-13 13:24:52 +0700

    StartApp

---
+++ Array.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Using Array  นี่คือการใข้ตัวแปรอาร์เรย์
+// Define variable and value
+//Implicit
+let intArrayNumber = [11,22,33,44]
+
+//Explicit
+var intArrayNumber2 = [Int]()
+intArrayNumber2 = [55,66,77,88,13.4]
+
+//String type
+var strArrayName = ["Kaika","หมู","หมา"]
+var strArrayName2 = [String]()
+strArrayName2 = ["Jusmin","กุหลาบ","จำปี"]
+
+//Double
+var douArrayNum3 = [1.2,1.3,1.4,5]
+var douArrayNum4 = [Double]()
+douArrayNum4 = [2.1,2.2,2.3]
+ 
+
+
+		
